90ab6943e77834a8b4b8b824253370e7ae3cf17f
titus fx <titusfx@gmail.com>
2022-12-21 02:55:40 +0100

    feat(rewards): add repository adapter for rewards groups

--- RewardMatic 4000/RewardMatic 4000/Reward.cs
+++ RewardMatic 4000/RewardMatic 4000/Reward.cs
@@ -1,29 +1,57 @@
+#nullable enable
+using Newtonsoft.Json;
+
 namespace RewardMatic_4000
 {
     public class Reward
     {
-        public static Reward[] AvailableRewards = new Reward[]
-        {
-            new Reward(200, "Starting strong!"),
-            new Reward(300, "Getting better!"),
-            new Reward(400, "Nice job!"),
-            new Reward(500, "Keep going for cake!"),
-            new Reward(600, "OK, there's no cake. Keep going anyway!"),
-            new Reward(700, "YOU FINISHED! You are the only person to do this."),
-        };
+        public static Reward[] AvailableRewards = RepositoryReward.allRewards();
 
-        // the score you need to attain to get the reward
-
-        // the reward message
-
-        private Reward(int scoreDifferential, string message)
+        public Reward(int scoreDifferential, string message)
         {
             ScoreDifferential = scoreDifferential;
             Message = message;
         }
 
+        [JsonProperty("scoredifferential")]
         public int ScoreDifferential { get; }
 
+        [JsonProperty("name")]
         public string Message { get; }
+
+        public override bool Equals(object? otherReward)
+        {
+            if (otherReward == null || !(otherReward is Reward))
+            {
+                return false;
+            }
+
+            Reward castedReward = (Reward)otherReward;
+            return castedReward == this;
+        }
+
+        public static bool operator ==(Reward? lhs, Reward? rhs)
+        {
+            // Check if lhs or rhs is null
+            if (ReferenceEquals(lhs, null) || ReferenceEquals(rhs, null))
+            {
+                return false;
+            }
+
+            // Compare the fields of lhs and rhs
+            return lhs.Message == rhs.Message && lhs.ScoreDifferential == rhs.ScoreDifferential;
+        }
+
+        public static bool operator !=(Reward lhs, Reward rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override int GetHashCode()
+        {
+            // Combine the hash codes of the fields using the XOR operator
+            return this.Message.GetHashCode() ^ this.ScoreDifferential.GetHashCode();
+        }
+
     }
 }
